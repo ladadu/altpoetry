4548c18f46bad65d97ccf508ae7c2d69fdcd64a8
Alt Poetry <altpoetry@alt.poe.try>
2026-07-28 16:06:39 -0400

    gallery/ARH_PROSTRANSTVA

--- Aaa/conv_fotki.sh
+++ Aaa/conv_fotki.sh
@@ -2,8 +2,8 @@ echo "==== Konvertiruem papku [Aaa] ====="
 mkdir -p Aaa/{img,tm}
 for f in Aaa/*.jpg ; do n=$( echo $f | sed 's/^Aaa\///' )
 echo -n "[$n] --> [img/$n]"
-magick $f -strip -interlace Plane -quality 60 -rotate 90 Aaa/img/$n
+magick $f -strip -interlace Plane -quality 60 -rotate 0 Aaa/img/$n
 echo -n " + [tm_$n]"
-magick $f -strip -interlace Plane -quality 60 -rotate 90 -resize "x247>" Aaa/tm/tm_$n
+magick $f -strip -interlace Plane -quality 60 -rotate 0 -resize "x247>" Aaa/tm/tm_$n
 echo " Gotovo!"
 done
